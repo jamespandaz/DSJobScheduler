5bdfe2315fc31f82f3c8e48f9b2e92ff823b6c2e
James Coulson <61818136+jamespandaz@users.noreply.github.com>
2022-04-03 10:17:48 +1000

    better server sorting

--- MyClient.java
+++ MyClient.java
@@ -1,6 +1,14 @@
 import java.io.*;  
 import java.net.*;
-import java.util.Arrays;
+import java.io.File;
+import java.util.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
 
 public class MyClient {  
     public static void main(String[] args) {  
@@ -9,7 +17,7 @@ public class MyClient {
             BufferedReader dis = new BufferedReader(new InputStreamReader(socket.getInputStream())); //buffer reader to read 
             DataOutputStream dout = new DataOutputStream(socket.getOutputStream());  //output stream to output text
             String str;
-            String currJob;
+            String[] currJob;
             String getsData;
             int nRecs;
 
@@ -27,47 +35,44 @@ public class MyClient {
             System.out.println("SERVER: "+str);
             dout.flush();
 
-            int jobNo = 0;
+            if (str.equals("OK")){
+                Document doc = readFile("./ds-system.xml"); // read the ds system for server info
+
+                NodeList nList = doc.getElementsByTagName("server"); // find server names
+                String[][] servers = getServerList(nList); //put them in list
+                
+                int max_core_count = Integer.MIN_VALUE; // storage for max core count
+                String server_max = "";
+                String server_id = "";
+                
+                for(int i = 0; i< servers.length; i++) { //go through list and try to find which is biggest
+                    if (Integer.parseInt(servers[i][1]) > max_core_count) {
+                        max_core_count = Integer.parseInt(servers[i][1]);
+                        server_max = servers[i][0];
+                        server_id = "0";
+                    }
+                }
+            }
+
             while (!str.equals("NONE")){ // check if the message is not none
                 dout.write(("REDY\n").getBytes()); //send ready handshake
                 dout.flush();
     
                 str = dis.readLine();  //receive
                 System.out.println("SERVER: "+str);
-                currJob = str;
-                dout.flush();
-                
-                dout.write(("GETS All\n").getBytes()); // get server information
-                dout.flush();
-                str = dis.readLine();  //receive
-                System.out.println("SERVER: "+str);
-                
-                getsData = str; // store the gets All data into a string
-                nRecs = Character.getNumericValue(getsData.charAt(5)); // read data and get the nRecs (number of servers)
+                currJob = str.split(" ",0);
                 dout.flush();
 
-                dout.write(("OK\n").getBytes()); // acknlowedge the data
-                dout.flush();
-                
-                String[] serverList = new String[nRecs]; // make array as big as the number of servers
 
-                for (int i = 0; i < nRecs; i++){ // loop for the amount for servers
+                while (currJob[0].equals("JCPL")) {    // check if jcpl if there is 
+                    dout.write(("REDY\n".getBytes()));   // then ignore
                     str = dis.readLine();
-                    serverList[i] = str; // write server info into array
-                    System.out.println("SERVER: "+str);
-                    dout.flush();
+                    currJob = str.split(" ");
                 }
-                
-                int serverNumber = serverList.length;
 
-                dout.write(("OK\n").getBytes()); // acknlowedge the data
-                dout.flush();
-
-                str = dis.readLine();  //receive
-                System.out.println("SERVER: "+str);
-                dout.flush();
-
-                dout.write(("SCHD "+jobNo+serverList[serverNumber]+"\n").getBytes());
+                if (str.equals("NONE")) { //if there are no more jobs left then leave while loop
+                    break;
+                }
 
                 dout.write(("OK\n").getBytes()); // acknlowedge the data
                 dout.flush();
@@ -88,5 +93,36 @@ public class MyClient {
             socket.close();  
         }catch(Exception e){System.out.println(e);}  
     
-    }  
+    }
+
+    public static Document readFile(String path) {
+        try {
+            File file = new File(path);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document document = db.parse(file);
+            document.getDocumentElement().normalize();
+            return document;
+        }
+
+        catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public static String[][] getServerList(NodeList nList){
+        String[][] xml = new String[nList.getLength()][2];
+            for(int i =0 ; i< nList.getLength(); i++){
+                Node nNode = nList.item(i);
+                if(nNode.getNodeType()== Node.ELEMENT_NODE){
+                    Element eElement = (Element) nNode;
+                    xml[i][0]=eElement.getAttribute("type");
+                    xml[i][1]= eElement.getAttribute("coreCount").toString();
+                }
+            }
+
+
+        return xml;
+    }
 }  
